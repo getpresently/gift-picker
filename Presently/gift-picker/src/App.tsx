d0afde73a89e3899e9ec653457479bd44872c66b
QLangstaff <qlangsta@uwo.ca>
2021-07-23 22:35:27 -0400

    Add existing file

---
+++ Presently/gift-picker/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">GIFT PICKER 🎁</h1>
+        <p>By <a href="https://getpresently.com" target="_blank" rel="noreferrer">Presently</a> 💜</p>
+        <p>Helping you pick the perfect gift for any occasion</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
