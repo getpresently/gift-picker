121d339f435ecab44c71892c96b64c484199ad27
vrushalitarte01 <tarte.v@northeastern.edu>
2021-11-16 00:22:49 -0500

    fixed banner padding

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,12 @@
 function Banner() {
   return (
-    <p>
-      We're live on&nbsp;{" "}
-      <a href="https://www.producthunt.com/">ProductHunt!</a> &nbsp;Give us a
-      vote and join our mailing list for new feature notifications
-    </p>
+    <div id="bannerText">
+      <p>
+        We're live on&nbsp;
+        <a href="https://www.producthunt.com/">ProductHunt!</a>&nbsp;Give us a
+        vote and join our mailing list for new feature notifications
+      </p>
+    </div>
   );
 }
 
