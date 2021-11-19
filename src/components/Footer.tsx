2bb0aaa44dc9415232d9d6accaf64e15b7795fb6
marc <marc.bacvanski@gmail.com>
2021-11-18 19:40:20 -0500

    Make header and footer logos clicky

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
-import logo from '../footer_logo.svg'
-import signature from '../presently_word.png'
+import {useEffect, useState} from 'react';
+import logo from '../footer_logo.svg';
+import signature from '../presently_word.png';
+import {Link} from 'react-router-dom';
 
 function Footer() {
 
@@ -16,10 +17,11 @@ function Footer() {
     <div className="text-white text-xs text-center sm:text-left flex p-4 md:pl-20">
       <div className="m-auto">
         <p className="font-bold">About GiftPicker</p>
-        <p>GiftPicker is a quick quiz that recommends amazing presents from our database of 1,000+ high-quality gifts!</p>
+        <p>GiftPicker is a quick quiz that recommends amazing presents from our database of 1,000+ high-quality
+          gifts!</p>
       </div>
     </div>
-  )
+  );
 
   return (
     <footer className="text-gray-600 w-screen body-font" id="footer">
@@ -33,9 +35,11 @@ function Footer() {
           </> :
           <>
             {about}
-            <div className="flex">
-              <img className="m-auto w-20 h-20 object-scale-down" src={logo} alt="Footer_Logo"></img>
-            </div>
+            <Link to="/home" className={'mt-8'}>
+              <div className="flex">
+                <embed className="m-auto w-20 h-20 object-scale-down" style={{pointerEvents: 'none'}} src={logo}/>
+              </div>
+            </Link>
           </>}
         <div className="flex grid grid-cols-1 p-3 md:p-10">
           <span
@@ -46,21 +50,23 @@ function Footer() {
             "
           >
             <a href="https://www.instagram.com/giftpicker.io/" className="text-gray-500 text-3xl">
-              <i className="fab fa-instagram" style={{ color: "white" }}></i>
+              <i className="fab fa-instagram" style={{color: 'white'}}/>
             </a>
             <a href="https://twitter.com/giftpicker_io" className="text-3xl">
-              <i className="fab fa-twitter" style={{ color: "white" }}></i>
+              <i className="fab fa-twitter" style={{color: 'white'}}/>
             </a>
             <a href="https://www.tiktok.com/@thebestgifts?" className="text-3xl text-white">
-              <i className="fab fab fa-tiktok" style={{ color: "white" }}></i>
+              <i className="fab fab fa-tiktok" style={{color: 'white'}}/>
             </a>
           </span>
-          <p className="font-bold text-sm text-white flex flex-row gap-2 justify-center pt-2">Powered by <img alt="presently" src={signature} className="w-16 h-6 object-scale-down"></img>
+          <p className="font-bold text-sm text-white flex flex-row gap-2 justify-center pt-2">Powered by
+            <Link to="/home">
+              <embed style={{pointerEvents: 'none'}} src={signature} className="w-16 h-6 object-scale-down"/>
+            </Link>
           </p>
-
         </div>
-      </div >
-    </footer >
+      </div>
+    </footer>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,30 @@
-import { Link } from 'react-router-dom';
-import React, { useState, useEffect } from "react";
+import {Link} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+
 function Header() {
-  const [header, setHeader] = useState("Header");
+  const [header, setHeader] = useState('Header');
   const listenScrollEvent = () => {
     if (window.scrollY < 103) {
-      return setHeader("Header");
+      return setHeader('Header');
     } else if (window.scrollY > 100) {
-      return setHeader("Header2");
+      return setHeader('Header2');
     }
   };
   useEffect(() => {
-    window.addEventListener("scroll", listenScrollEvent);
+    window.addEventListener('scroll', listenScrollEvent);
 
-    return () => window.removeEventListener("scroll", listenScrollEvent);
+    return () => window.removeEventListener('scroll', listenScrollEvent);
   }, []);
   return (
-    <div id={header}>
-      <a href="HomePage">
-      <Link to="/home">
-
-        <button type="button">
-          <img
+    <div>
+      <div id={header}>
+        <Link to="/home">
+          <embed
+            style={{pointerEvents: 'none'}}
             id="HeaderLogoButton"
-            src="./headerLogo.svg"
-            alt="logo button"
-          ></img>
-        </button>
+            src="./headerLogo.svg"/>
         </Link>
-      </a>
+      </div>
     </div>
   );
 }
