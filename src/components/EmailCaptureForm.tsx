c4ab3c7a77fbc7259ccca23e1d33a4efe2e0572a
Alex Reed <alexreed100@gmail.com>
2021-11-18 19:06:50 -0500

    add email capture

--- src/components/EmailCaptureForm.tsx
+++ src/components/EmailCaptureForm.tsx
@@ -2,10 +2,12 @@ const EmailCaptureComponent = () => {
   return (
     <div id="emailCaptureContainer">
       <h2>🎉 Drop your email for surprises</h2>
-      <form>
-        <button id="emailButton" type="submit">
-          Submit
-        </button>
+      <form className="emailCaptureForm">
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
+        <iframe title = "airtable" className="airtable-embed airtable-dynamic-height" 
+        src="https://airtable.com/embed/shreCiDTt7rOLSUdz?backgroundColor=greenLight" 
+        frameBorder="0" width="100%" height="470" 
+        style={{background: "transparent", border: "1px"}}></iframe>
       </form>
     </div>
   );
