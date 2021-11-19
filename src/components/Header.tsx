7ec48ab48f18d954ef3363ebde93afa3c6a9b264
Marc Bacvanski <marc.bacvanski@gmail.com>
2021-11-18 19:22:02 -0500

    Fix the header logo display (#27)
    * Fix the header logo display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,35 @@
-import { Link } from 'react-router-dom';
-import React, { useState, useEffect } from "react";
+import React, {useEffect, useState} from 'react';
+
 function Header() {
-  const [header, setHeader] = useState("Header");
+  const [header, setHeader] = useState('Header');
   const listenScrollEvent = () => {
     if (window.scrollY < 103) {
-      return setHeader("Header");
+      return setHeader('Header');
     } else if (window.scrollY > 100) {
-      return setHeader("Header2");
+      return setHeader('Header2');
     }
   };
   useEffect(() => {
-    window.addEventListener("scroll", listenScrollEvent);
+    window.addEventListener('scroll', listenScrollEvent);
 
-    return () => window.removeEventListener("scroll", listenScrollEvent);
+    return () => window.removeEventListener('scroll', listenScrollEvent);
   }, []);
   return (
-    <div id={header}>
-      <a href="HomePage">
-      <Link to="/home">
+    <div>
+      <embed
+        id="HeaderLogoButton"
+        src="./headerLogo.svg"
+        // alt="logo button"
+      />
 
-        <button type="button">
-          <img
-            id="HeaderLogoButton"
-            src="./headerLogo.svg"
-            alt="logo button"
-          ></img>
-        </button>
-        </Link>
-      </a>
+      {/*<div id={header}>*/}
+        {/*<a href="HomePage">*/}
+        {/*<Link to="/home">*/}
+        {/*<div>*/}
+        {/*</div>*/}
+        {/*</Link>*/}
+        {/*</a>*/}
+      {/*</div>*/}
     </div>
   );
 }
