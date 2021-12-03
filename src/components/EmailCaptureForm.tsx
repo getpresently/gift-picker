b436c1528497761ba8ed54a09bba1785a259c12d
Nikola Dobrev <dnikola43@gmail.com>
2021-12-02 19:03:00 -0500

    changed email form to tailwind form still need finish ui

--- src/components/EmailCaptureForm.tsx
+++ src/components/EmailCaptureForm.tsx
@@ -2,12 +2,20 @@ const EmailCaptureComponent = () => {
   return (
     <div id="emailCaptureContainer">
       <h2>🎉 Drop your email for surprises</h2>
-      <form className="emailCaptureForm">
-        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
-        <iframe title = "airtable" className="airtable-embed airtable-dynamic-height" 
-        src="https://airtable.com/embed/shreCiDTt7rOLSUdz?backgroundColor=greenLight" 
-        frameBorder="0" width="100%" height="470" 
-        style={{background: "transparent", border: "1px"}}></iframe>
+      <form className="w-full max-w-sm justify-center items-center">
+        <div className="flex items-center border-b border-teal-500 py-2 justify-center">
+          <input
+            className="text-white appearance-none bg-transparent border-none w-full text-white-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="email"
+            placeholder="Email"
+          />
+          <button
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="button"
+          >
+            Sign Up
+          </button>
+        </div>
       </form>
     </div>
   );
