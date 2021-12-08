6951397be9136e07920792efc37f0be59a382ff3
Kris Amerman <amerman.k@northeastern.edu>
2021-12-08 13:57:51 -0500

    started on form

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 import Popup from 'reactjs-popup';
 import "./Modal.css";
 import { useEffect, useState } from "react";
-import { Form, Button} from 'react-bootstrap';
+import { Form, Button, Row, Col, FloatingLabel } from 'react-bootstrap';
 
 function Modal() {
     return (
         <Popup trigger={<button className="button"> 💬 </button>} modal>
             <div id="container">
-        
+
             </div>
             <div id="content">
-                    <p>Give Us Feedback!</p>
-                    
+                <p>Give Us Feedback!</p>
+                <FloatingLabel controlId="floatingSelect" label="Works with selects">
+                    <Form.Select aria-label="Floating label select example">
+                        <option>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </Form.Select>
+                </FloatingLabel>
+
+                <FloatingLabel controlId="floatingTextarea" label="Comments" className="mb-3">
+                    <Form.Control as="textarea" placeholder="Leave a comment here" />
+                </FloatingLabel>
             </div>
         </Popup>
     );
