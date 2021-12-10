8c604d76304592805388dc58f3fd4afa95a91532
Neel <nsortur@yahoo.com>
2021-12-09 22:28:22 -0500

    rounded input

--- src/components/EmailCaptureForm.tsx
+++ src/components/EmailCaptureForm.tsx
@@ -22,17 +22,17 @@ const EmailCaptureComponent = () => {
     <div id="emailCaptureContainer" className="items-center justify-center">
       <h2>🎉 Drop your email for surprises</h2>
       <form className="w-full max-w-xxs md:min-w-full justify-center" onSubmit={handleSubmit}>
-        <div className="flex items-center border-b border-teal-500 py-2 justify-center">
+        <div className="flex items-center border-teal-500 pt-5 justify-center">
           <input
-            className="text-white appearance-none bg-transparent border-none w-full text-white-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            className="text-white appearance-none bg-transparent border border-white w-full text-white-700 mr-3 py-3 px-3 h-14 rounded-xl focus:outline-none"
             type="email"
             value={email}
             onChange={handleChange}
             placeholder="Your email"
           />
           <button
-            className="bg-gray-900 w-52 h-11 hover:bg-black text-white button_nav" >
-            Sign up
+            className="bg-gray-900 w-52 h-14 hover:bg-black text-white button_nav" >
+            Submit
           </button>
         </div>
       </form>
