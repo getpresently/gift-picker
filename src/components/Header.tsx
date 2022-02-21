652682e9b69aa1710b081476a3dbac82aa61998c
Evan Cook <cook.evank@gmail.com>
2022-02-21 16:17:30 -0500

    edit svg file to fix alignment and scaling of logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,30 @@
-import React, {useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 function Header() {
-  const [header, setHeader] = useState('Header');
+  const [header, setHeader] = useState("Header");
   const listenScrollEvent = () => {
     if (window.scrollY < 103) {
-      return setHeader('Header');
+      return setHeader("Header");
     } else if (window.scrollY > 100) {
-      return setHeader('Header2');
+      return setHeader("Header2");
     }
   };
   useEffect(() => {
-    window.addEventListener('scroll', listenScrollEvent);
+    window.addEventListener("scroll", listenScrollEvent);
 
-    return () => window.removeEventListener('scroll', listenScrollEvent);
+    return () => window.removeEventListener("scroll", listenScrollEvent);
   }, []);
   return (
     <div>
       <div id={header}>
         <Link to="/">
           <img
-            style={{pointerEvents: 'none' }}
+            style={{ pointerEvents: "none" }}
             id="HeaderLogoButton"
-            src="./headerLogo.svg"  alt={'logo'}/>
+            className="h-24 max-h-full mb-20 lg:mb-28 xl:mb-40 object-fill"
+            src="./headerLogo.svg"
+            alt={"logo"}
+          />
         </Link>
       </div>
     </div>
